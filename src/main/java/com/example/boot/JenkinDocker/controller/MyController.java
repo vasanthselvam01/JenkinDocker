f19458a85739bcbcbd6c1136743288efe3c0221c
vasanthselvam01 <vasanthselvam01@gmail.com>
2023-10-18 18:54:38 +0530

    controller class get mapping has updated

--- src/main/java/com/example/boot/JenkinDocker/controller/MyController.java
+++ src/main/java/com/example/boot/JenkinDocker/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @GetMapping(value = "/cities")
+    @GetMapping(value = "/city")
     public String getCities() {
         return "Chennai";
     }
